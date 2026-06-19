02f49ab64a0c95245c9883698340226b65b2d828
athu0411 <thunee0411@gmail.com>
2026-06-19 21:16:24 +0700

    feat: implement document reviews and rating backend

--- backend/src/main/java/com/example/keeper/systems/document/dto/request/DocumentReviewRequest.java
+++ backend/src/main/java/com/example/keeper/systems/document/dto/request/DocumentReviewRequest.java
@@ -1,4 +1,9 @@
 package com.example.keeper.systems.document.dto.request;
 
+import lombok.Data;
+
+@Data
 public class DocumentReviewRequest {
+    private Integer rating;
+    private String comment;
 }
